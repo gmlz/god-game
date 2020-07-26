b9ae864fbe8fc7e61aea5ce131cf046f3f631689
gmlz <54523935+gmlz@users.noreply.github.com>
2020-07-26 18:41:08 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 var gameData = {
-    men: 0
+    men: {
+        current: 0,
+        max: 1
+    },
+    food: {
+        current: 10
+    }
 }
   
 function createMan() {
-    gameData.men += 1;
-    document.getElementById("men").innerHTML = "Men: " + gameData.men
+    if ( gameData.men.current + 1 <= gameData.men.max) {
+        gameData.men.current += 1;
+        document.getElementById("men").innerHTML = "Men: " + gameData.men.current + "/" + gameData.men.max;
+        if( gameData.men.current == 1 ) {
+            document.getElementById("food").innerHTML = "Food: 10 (-1/day)";
+            document.getElementById("btn_forage").style.display = "block";
+        }
+    }
+    if ( gameData.men.current >= gameData.men.max) {
+        document.getElementById("btn_createMan").disabled = true;
+    }
+}
+
+function forage() {
+    gameData.food.current += 1;
+    document.getElementById("food").innerHTML = "Food: " + gameData.food.current + " (-1/day)";
 }
