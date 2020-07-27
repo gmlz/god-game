85a01648380a56613585c31f217cb3f9d4a234e4
gmlz <54523935+gmlz@users.noreply.github.com>
2020-07-27 17:28:53 -0500

    forage experience

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
 var gameData = {
-    men: {
-        current: 0,
-        max: 1
-    },
+    men: 0,
     food: 0,
-    foragers: 0
+    foragers: 0,
+    forage_exp: 0,
+    forage_lvl: 0,
+    forage_exp_goal: 10
 }
   
 function createMan() {
-    if ( gameData.men.current + 1 <= gameData.men.max) {
-        gameData.men.current += 1;
-        document.getElementById("men").innerHTML = "Men: " + gameData.men.current + "/" + gameData.men.max;
-        if( gameData.men.current == 1 ) {
+    if ( gameData.men == 0 ) {
+        gameData.men += 1;
+        document.getElementById("men").innerHTML = "Men: " + gameData.men + "/1";
+        if( gameData.men == 1 ) {
             document.getElementById("btn_forage").style.display = "block";
+            gameData.food = 10;
+            gameData.foragers = 0;
         }
     }
-    if ( gameData.men.current >= gameData.men.max) {
+    if ( gameData.men >= 1) {
         document.getElementById("btn_createMan").disabled = true;
     }
 }
 
 function forage() {
     gameData.foragers += 1;
-    document.getElementById("food").innerHTML = "Food: " + gameData.food;
-    document.getElementById("btn_forage").disabled = true;
+    document.getElementById("food").innerHTML = "Food: " + gameData.food;    
+    document.getElementById("btn_forage").disabled = true;  
 }
 
 function tick() {
-    if ( gameData.foragers > 0 ) {
-        gameData.food += gameData.foragers;
-        document.getElementById("food").innerHTML = "Food: " + gameData.food;
+    var food_rate = gameData.foragers * (1 + 0.1 * gameData.forage_lvl) - gameData.men;
+    gameData.food += food_rate;
+    if ( gameData.men > 0 ) {
+        document.getElementById("food").innerHTML = "Food: " + gameData.food + " (" + food_rate + ")";
+    }
+    gameData.forage_exp += gameData.foragers;
+    if ( gameData.forage_exp > 0 ) {
+        document.getElementById("forage_exp").innerHTML = "exp: " + gameData.forage_exp + "/" + gameData.forage_exp_goal + " (" + gameData.foragers + ")";
+    }
+    if ( gameData.forage_exp >= gameData.forage_exp_goal ) {
+        gameData.forage_lvl += 1;
+        gameData.forage_exp_goal *= 2;
     }
 }
 
 var mainGameLoop = window.setInterval(function() {
     tick()
-  }, 1000)
+}, 1000)
